d36e18927bd322cf591e519beffac2468178bbc2
Giampaolo Fois <giampaolo.fois@softfobia.com>
2020-05-14 17:03:25 +0200

    test meta

---
+++ resources/views/meta_test.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <style data-next-hide-fouc="true">
+        body {
+            display: none
+        }
+    </style><noscript data-next-hide-fouc="true">
+        <style>
+            body {
+                display: block
+            }
+        </style>
+    </noscript>
+    <meta name="viewport" content="width=device-width" />
+    <meta charSet="utf-8" />
+    <title>Regione Autonoma della Sardegna - Fase 2, la Sardegna riparte. Il Presidente Solinas: atto di fiducia verso i sardi</title>
+    <meta name="description" content="E la Sardegna dunque riparte, con la ripresa di molte attività interrotte a causa della pandemia. 
+Lo prevede la nuova Ordinanza emessa dal Presidente della Regione, frutto del costante dialogo con gli scienziati del Comitato tecnico scientifico. Un’ordinanza che, ha detto il Presidente Solinas, rappresenta un atto di fiducia nei confronti del Popolo Sardo, che ha dimostrato grande senso di responsabilità nell’osservanza delle precedenti misure restrittive." />
+    <meta property="og:type" content="website" />
+    <meta name="og:title" property="og:title" content="Fase 2, la Sardegna riparte. Il Presidente Solinas: atto di fiducia verso i sardi" />
+    <meta name="og:description" property="og:description" content="E la Sardegna dunque riparte, con la ripresa di molte attività interrotte a causa della pandemia. 
+Lo prevede la nuova Ordinanza emessa dal Presidente della Regione, frutto del costante dialogo con gli scienziati del Comitato tecnico scientifico. Un’ordinanza che, ha detto il Presidente Solinas, rappresenta un atto di fiducia nei confronti del Popolo Sardo, che ha dimostrato grande senso di responsabilità nell’osservanza delle precedenti misure restrittive." />
+    <meta property="og:site_name" content="Proper Noun" />
+    <meta property="og:url" content="undefined" />
+    <meta property="og:locale" content="it_IT" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="Fase 2, la Sardegna riparte. Il Presidente Solinas: atto di fiducia verso i sardi" />
+    <meta name="twitter:description" content="E la Sardegna dunque riparte, con la ripresa di molte attività interrotte a causa della pandemia. 
+Lo prevede la nuova Ordinanza emessa dal Presidente della Regione, frutto del costante dialogo con gli scienziati del Comitato tecnico scientifico. Un’ordinanza che, ha detto il Presidente Solinas, rappresenta un atto di fiducia nei confronti del Popolo Sardo, che ha dimostrato grande senso di responsabilità nell’osservanza delle precedenti misure restrittive." />
+    <meta name="twitter:site" content="@propernounco" />
+    <meta name="twitter:creator" content="@propernounco" />
+    <link rel="icon" type="image/png" href="/favicon.ico" />
+    <link rel="apple-touch-icon" href="/favicon.ico" />
+    <meta property="og:image" content="https://www.propernoun.co/static/images/proper-noun-social.png" />
+    <meta name="next-head-count" content="18" />
+</head>
+<body>
+Ciao
+</body>
+</html>
